d132dfbff070b00e0de8fa32647ba1f58d953d8f
matsura <eldar32@gmail.com>
2017-05-23 23:10:27 +0200

    Fixed issues with importing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app works!';
   items = [{ id: '1', name: '2' }, { id: '3', name: '4' }, { id: '5', name: '6' }];
+
+  constructor() {
+    this.items = [...this.items];
+  }
 }
